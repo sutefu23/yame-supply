d76ad9f9110b9850604d59e2d78a67c13773d2e6
sutefu23 <sutefu23@gmail.com>
2022-12-29 11:36:41 +0900

    is_exportedフィールド追加

--- html/database/migrations/2022_09_09_051637_create_buildinginfo_table.php
+++ html/database/migrations/2022_09_09_051637_create_buildinginfo_table.php
@@ -18,7 +18,6 @@ return new class extends Migration
             $table->string("field_name",255)->comment("現場名")->nullable();
             $table->foreignId("builder_user_id")->comment("工務店ユーザーID")->constrained('users');
             $table->date("export_expected_date")->comment("出荷予定日");
-            $table->boolean("is_exported")->comment("出荷済")->default(false);
             $table->foreignId("create_user_id")->comment("登録者")->constrained('users');
             $table->foreignId("update_user_id")->comment("変更者")->constrained('users');
             $table->timestamps();
