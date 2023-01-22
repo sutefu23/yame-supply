9c852907454be7f27c4f20a9bec7d9bce43c3dfe
sutefu23 <sutefu23@gmail.com>
2023-01-22 17:37:18 +0900

    出荷情報の棟情報IDへの外部キーnullableに

--- html/database/migrations/2022_12_28_182111_modify_outstockInfo.php
+++ html/database/migrations/2022_12_28_182111_modify_outstockInfo.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('OutStockInfo', function (Blueprint $table) {
-            $table->foreignId("building_info_id")->nullable()->constrained()->after('reason')->comment("棟情報ID");
+            $table->foreignId("building_info_id")->nullable()->constrained('BuildingInfo')->after('reason')->comment("棟情報ID");
         });
     }
 
